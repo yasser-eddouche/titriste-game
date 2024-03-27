d17f473d281b2bf144faef9c8eb5d82af404b6de
yasedd <eddouchey@gmail.com>
2024-03-27 11:19:27 +0000

    insert update

--- game/game.cpp
+++ game/game.cpp
@@ -20,67 +20,6 @@ LstPieces::~LstPieces() {
 }
 
 
-
-
-// LstPieces* LstPieces::insertPieceLeft(LstPieces *list, Color color, Shapes shape) {
-//     // Crée une nouvelle pièce
-//     Piece newPiece;
-//     newPiece.color = color;
-//     newPiece.shape = shape;
-
-//     // Crée un nouveau nœud pour la nouvelle pièce
-//     LstPieces *newNode = new LstPieces();
-//     newNode->piece = new Piece(newPiece);
-
-//     // Vérifie si la liste est vide
-//     if (list == nullptr) {
-//         newNode->next = newNode; // Nouveau nœud pointe vers lui-même car il est le seul nœud dans la liste
-//         return newNode; // Retourne le nouveau nœud car c'est maintenant la tête de la liste
-//     }
-
-//     // Si la liste n'est pas vide, insère le nouveau nœud à gauche de la pièce actuelle
-//     LstPieces *current = list;
-//     // while (current->next != list) {
-//         current = current->next; // Avance jusqu'au dernier nœud de la liste
-//     // }
-//     // Insère le nouveau nœud après le dernier nœud actuel
-//     list=newNode;// Nouveau nœud pointe vers la tête de la liste
-//     newNode->next=current;
-
-//     printf("Piece inserted left\n");
-//     return newNode; // Retourne le nouveau nœud inséré
-// }
-
-// LstPieces *LstPieces::insertPieceRight(LstPieces *list, Color color, Shapes shape) {
-//     // Crée une nouvelle pièce
-//     Piece newPiece;
-//     newPiece.color = color;
-//     newPiece.shape = shape;
-
-//     // Crée un nouveau nœud pour la nouvelle pièce
-//     LstPieces *newNode = new LstPieces();
-//     newNode->piece = new Piece(newPiece);
-
-//     // Vérifie si la liste est vide
-//     if (list == nullptr) {
-//         newNode->next = newNode; // Nouveau nœud pointe vers lui-même car il est le seul nœud dans la liste
-//         return newNode; // Retourne le nouveau nœud car c'est maintenant la tête de la liste
-//     }
-
-//     // Si la liste n'est pas vide, insère le nouveau nœud à droite de la pièce actuelle
-//    // Trouve le dernier nœud de la liste (queue)
-//     LstPieces *tail = list;
-//     while (tail->next != list) {
-//         tail = tail->next;
-//     }
-
-//     // Insère le nouveau nœud après le dernier nœud actuel
-//     tail->next =newNode;
-//     newNode->next = list; 
-
-//     printf("Piece inserted Right\n");
-//     return newNode; // Retourne le nouveau nœud inséré
-// }
 void LstPieces::insertPieceRight(LstPieces **list, Color color, Shapes shape) {
     // Créer une nouvelle pièce
     Piece newPiece;
@@ -149,7 +88,6 @@ void LstPieces::printList(LstPieces *list){
     }
     printf("Printing list\n");
 
-    //  cout << "Piece: " << current->piece->color << " " << current->piece->shape << endl;
 }
 
 Color getColorFromString(const string& colorStr) {
